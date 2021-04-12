19ded2446663eb868e05bb42708b8ab447286298
Karem Cristine de Souza <karem@clickmulti.com.br>
2021-04-11 21:27:06 -0300

    commit final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="hello">
+    <datepicker
+      v-model="date"
+      :format="DatePickerFormat"
+      :disabledDates="disabledDates"
+       :displayFormat="'DD.MM.YYYY'"
+       :first-day-of-week="1"
+       calendar-class="my_calendar"
+       input-class="textfield"
+       :language="ptBR"
+    >
+    </datepicker>
   </div>
 </template>
 
 <script>
+import Datepicker from "vuejs-datepicker";
+import {ptBR} from 'vuejs-datepicker/dist/locale'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    Datepicker,
+  },
+
+  data() {
+    return {
+      ptBR:ptBR,
+       date: "",
+       data: "",
+      
+      DatePickerFormat: "dd/MM/yyyy",
+      disabledDates: {
+        // to: new Date(Date.now() - 8640000),
+        daysOfMonth: []
+      },
+       
+      
+    };
+  },
+  methods:{
+    setDays(){
+      this.disabledDates.daysOfMonth = [1,2,3,4,5,6,7,8,9,10,11,12]
+      // var date = new Date()
+      // this.model.date = date.toLocaleDateString()
+    }
+  },
+  mounted(){
+    this.setDays()
+       var date = new Date()
+      this.date1 = date.toLocaleDateString()
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style >
+.textfield{
+ top: 100px;
+margin-left: 600px;
+width: 365px;
+height: 38px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #CED4DA;
+border-radius: 4px !important;
+opacity: 1;
+font-size: 20px;
+color:#707a85 ;
+outline: none;
+text-align: left;
+font: normal normal normal 20px/32px Poppins;
+letter-spacing: 0px;
+color: #9A9A9A;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.my_calendar{
+top: 40px;
+margin-left: 600px;
+width: 365px !important;
+height: 38px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #CED4DA  !important;
+border-radius: 1px !important;
+font: normal normal normal 20px/32px Poppins;
+letter-spacing: 0px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.vdp-datepicker__calendar .cell.day-header {
+    font-size: 75%;
+    white-space: nowrap;
+    cursor: inherit;
+    color: #2CD889;
 }
-a {
-  color: #42b983;
+.vdp-datepicker__calendar header span {
+    display: inline-block;
+    text-align: center;
+    width: 71.42857142857143%;
+    float: left;
+    color: #2CD889;
+}
+.vdp-datepicker__calendar {
+    position: absolute;
+    z-index: 100;;
+    width: 300px;
+    height: 300px;
+    border: 1px solid #ccc;
+}
+.vdp-datepicker * {
+    box-sizing: border-box;
+    font-weight: bold;
 }
 </style>
